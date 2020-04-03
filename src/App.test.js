9ade921513512271bd829754f29f3ad85ccd6573
Georgie Kirschner <georgie@montanacodeschool.com>
2020-04-03 09:07:57 -0600

    adjust main component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("<App /> - Renders Heading", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = getByText("Georgie's Beer Favorites");
+  expect(heading).toBeInTheDocument();
 });
